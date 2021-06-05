fbbf36e4a9579b8ad2d203ace1ce1e71f69e9f56
SanjayKarmaDevelopers <83385569+SanjayKarmaDevelopers@users.noreply.github.com>
2021-06-05 18:54:29 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Note(){
+    return {
+        <div className="note">
+            <h1>Javascript and React.js</h1>
+            <p>This was an amazing bootcamp taken up byShaurya sinha.We covered Everything from Scretch including Javascript and React js and HTML
+            </p>
+        </div>
+    };
+}
+
+export default Header; 
